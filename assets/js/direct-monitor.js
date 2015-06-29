21ce52fa253b9c6a741582de61d9b3e7091ef583
scott-riley <hugs@scottriley.im>
2015-06-29 15:10:26 -0400

    Made a slightly real delay

--- assets/js/direct-monitor.js
+++ assets/js/direct-monitor.js
@@ -8,17 +8,30 @@ function gotStream(stream) {
 	// make a new fuckin audio context yo
 
 	// make an audio node so we can use it for the streaming
-	var mediaStreamSource = audioContext.createMediaStreamSource(stream)
+	var mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
 	// create a delay that we will later apply to the source
-	var delay = audioContext.createDelay()
-	delay.delayTime.value = 0.5;
+	var delay = audioContext.createDelay();
+	delay.delayTime.value = 0.4;
+
+	// set up feedback so the delay doesn’t just sound like wank
+	var feedback = audioContext.createGain();
+	feedback.gain.value = .64;
+
+	// set up a low-pass filter to warm signal as it feeds back
+	var lowPass = audioContext.createBiquadFilter(); // low-pass is default for biquad filer
+	lowPass.frequency.value = 900;
+
+	// feedback loop so audio softens out
+	delay.connect(feedback);
+	feedback.connect(lowPass);
+	feedback.connect(delay);
 
 	// connect it to destination so we can hear what’s coming in
-	mediaStreamSource.connect(delay)
-	mediaStreamSource.connect(audioContext.destination)
-	delay.connect(audioContext.destination)
+	mediaStreamSource.connect(delay);
+	mediaStreamSource.connect(audioContext.destination);
+	delay.connect(audioContext.destination);
 }
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-navigator.getUserMedia( { audio: true}, gotStream, function() { console.log("fucked up"); })
+navigator.getUserMedia( { audio: true}, gotStream, function() { console.log("fucked up"); });
