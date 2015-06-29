cc5472fa9c6865ee573a28884715411504129a0a
scott-riley <hugs@scottriley.im>
2015-06-29 14:57:35 -0400

    Super simple delay

--- assets/js/direct-monitor.js
+++ assets/js/direct-monitor.js
@@ -10,8 +10,14 @@ function gotStream(stream) {
 	// make an audio node so we can use it for the streaming
 	var mediaStreamSource = audioContext.createMediaStreamSource(stream)
 
+	// create a delay that we will later apply to the source
+	var delay = audioContext.createDelay()
+	delay.delayTime.value = 0.5;
+
 	// connect it to destination so we can hear what’s coming in
+	mediaStreamSource.connect(delay)
 	mediaStreamSource.connect(audioContext.destination)
+	delay.connect(audioContext.destination)
 }
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
