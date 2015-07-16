2321bfe748158304c9b33768556155c7d06600f1
scott-riley <hugs@scottriley.im>
2015-07-17 00:02:41 +0100

    Made a fuckin stereo delay

--- assets/js/direct-monitor.js
+++ assets/js/direct-monitor.js
@@ -11,26 +11,50 @@ function gotStream(stream) {
 	var mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
 	// create a delay that we will later apply to the source
-	var delay = audioContext.createDelay();
-	delay.delayTime.value = 0.4;
+	var delayLeft = audioContext.createDelay();
+	delayLeft.delayTime.value = 0.16;
+
+	var delayRight = audioContext.createDelay();
+	delayRight.delayTime.value = 0.28;
+
+	// create the panner nodes to spread the delay
+	var pannerLeft = audioContext.createStereoPanner();
+	pannerLeft.pan.value = -.8;
+
+	var pannerRight = audioContext.createStereoPanner();
+	pannerRight.pan.value = .8;
 
 	// set up feedback so the delay doesn’t just sound like wank
-	var feedback = audioContext.createGain();
-	feedback.gain.value = .64;
+	var feedbackLeft = audioContext.createGain();
+	feedbackLeft.gain.value = .84;
+
+	var feedbackRight = audioContext.createGain();
+	feedbackRight.gain.value = .24;
 
 	// set up a low-pass filter to warm signal as it feeds back
-	var lowPass = audioContext.createBiquadFilter(); // low-pass is default for biquad filer
-	lowPass.frequency.value = 900;
+	var lowPassLeft = audioContext.createBiquadFilter(); // low-pass is default for biquad filer
+	lowPassLeft.frequency.value = 900;
+
+	var lowPassRight = audioContext.createBiquadFilter(); // low-pass is default for biquad filer
+	lowPassRight.frequency.value = 700;
 
 	// feedback loop so audio softens out
-	delay.connect(feedback);
-	feedback.connect(lowPass);
-	feedback.connect(delay);
+	delayLeft.connect(feedbackLeft);
+	feedbackLeft.connect(lowPassLeft);
+	feedbackLeft.connect(delayLeft);
+
+	delayRight.connect(feedbackRight);
+	feedbackRight.connect(lowPassRight);
+	feedbackRight.connect(delayRight);
 
 	// connect it to destination so we can hear what’s coming in
-	mediaStreamSource.connect(delay);
+	mediaStreamSource.connect(delayLeft);
+	mediaStreamSource.connect(delayRight);
 	mediaStreamSource.connect(audioContext.destination);
-	delay.connect(audioContext.destination);
+	delayLeft.connect(pannerLeft);
+	delayRight.connect(pannerRight);
+	pannerLeft.connect(audioContext.destination);
+	pannerRight.connect(audioContext.destination);
 }
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
